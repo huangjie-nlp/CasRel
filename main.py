7f5caeb80f89150b95c6a7f7320d073851603ed2
huangjie-nlp <728155808@qq.com>
2023-02-16 20:21:01 +0800

    修改代码

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
--------------------------------------------------
-# @Project  :CasRel
-# @File     :train
-# @Date     :2021/7/24 22:28
-# @Author   :huangjie
-# @Email    :728155808@qq.com
-# @Software :PyCharm
--------------------------------------------------
-"""
-from model.casrel import Casrel
-from config.config import Config
 from framework.framework import Framework
+from config.config import Config
 import torch
-import numpy as np
 import random
+import numpy as np
 
-con = Config()
+seed = 2022
 
-seed = 1234
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 np.random.seed(seed)
@@ -26,11 +13,8 @@ random.seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-train_fn = "dataset/"+con.dataset+"/train_data.json"
-dev_fn = "dataset/"+con.dataset+"/val_data.json"
-
-model = Casrel(con)
-framework = Framework(con)
-framework.train(model, train_fn, dev_fn)
-print("start test......")
-framework.test(model, dev_fn)
+config = Config()
+fw = Framework(config)
+# fw.train()
+print("===================================test=============================")
+fw.test(config.test_data)
